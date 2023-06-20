c17c7bec5078ce8663a69287017252b158381d18
Jonatan Murcia <jonatanjosmurcia@gmail.com>
2023-06-20 08:42:05 -0500

    Pay methods with Kajita

--- assets/json/credentials.js
+++ assets/json/credentials.js
@@ -10,6 +10,29 @@ function credentials() {
     return currency ? credentials[currency] : '';
 }
 
+function privateCredentials() {
+    var credentials = {
+        CLP: '9f90b202cd254b2b85b0af8d18c96879',
+        COP: '3efe3f01cef54b4a8dd5d34ad275f6f8',
+        PEN: '1000000633843458299515659713079',
+        MXN: 'f647df83894f4648a77e70cb785c5888',
+        USD: 'fc96ef60f72e473191f535dc3db253a7'
+    }
+    const currency = localStorage.getItem('currency') ?? 'CLP';
+    return currency ? credentials[currency] : '';
+}
+function getIdForm() {
+    var ids = {
+        CLP: 'lLcDB2eh_',
+        COP: 'jbk4PyPOv',
+        PEN: 'IMROYl3xA',
+        MXN: 'yz79Tfrqt',
+        USD: 'LNMLVkzTW'
+    }
+    const currency = localStorage.getItem('currency') ?? 'CLP';
+    return currency ? ids[currency] : '';
+}
+
 function documentType() {
     var documentType = {
         CLP: 'RUT',
--- js/LoadCheckout.js
+++ js/LoadCheckout.js
@@ -1,4 +1,6 @@
 var totalG = 0;
+var cartProductsG = [];
+
 function loadCheckout(){
 
     var cartProducts = localStorage.getItem('cart') ? JSON.parse(localStorage.getItem('cart')) : []
@@ -16,10 +18,12 @@ function loadCheckout(){
                     </tr>`
     totalG = 0 + total;
     })
+    var merchantKey = credentials();
+    var idForm = getIdForm();
     var kushki = new KushkiCheckout({
-        kformId: "yz79Tfrqt",
+        kformId: idForm,
         form: "my-form",
-        publicMerchantId: "9fbf716eff3343ebbd77390132d0bf5e",
+        publicMerchantId: merchantKey,
         inTestEnvironment: true,
         amount: {
          subtotalIva: totalG * 0.81,
@@ -53,3 +57,164 @@ function loadCheckout(){
     $('#products-list').html(html);
 }
 
+function loadConfirm(){
+
+    var cartProducts = localStorage.getItem('cart') ? JSON.parse(localStorage.getItem('cart')) : []
+    const currency = localStorage.getItem('currency') ?? 'CLP';
+    var listProducts = [];
+    var total = 0;
+     cartProducts.forEach(product => {
+        let prodFilter = products().filter((ele)=> ele.reference == product.reference)[0]
+        total += product.quantity * prodFilter[currency]
+
+        listProducts.push({
+            id: product.reference,
+            title: product.name,
+            sku: product.reference,
+            price: prodFilter[currency],
+            quantity: product.quantity
+        })
+        totalG = 0 + total;
+    })
+
+    cartProductsG = [... listProducts]
+    console.log(cartProductsG);
+    
+}
+
+function payTransfer(token){
+    const currency = localStorage.getItem('currency') ?? 'CLP';
+    const privateKey = privateCredentials();
+    var payload = {
+        "token": token,
+        "amount": {
+          "currency": currency,
+          "subtotalIva": 0,
+          "subtotalIva0": (totalG * 0.81).toFixed(2),
+          "iva": (totalG * 0.19).toFixed(2),
+          "ice": 0
+        },
+        "fullResponse": "v2",
+        "ignoreWarnings": true,
+        "contactDetails": {
+          "firstName": "John",
+          "lastName": "Doe",
+          "email": "user@example.com",
+          "documentType": "CURP",
+          "documentNumber": "ABCD123456EF",
+          "phoneNumber": "+523988734644"
+        },
+        "orderDetails": {
+          "siteDomain": "kushkistore.com",
+          "shippingDetails": {
+            "name": "John Doe",
+            "phone": "+523988734644",
+            "addres": "Centro 123",
+            "city": "Monterrey",
+            "region": "Nuevo Leon",
+            "country": "Mexico"
+          },
+          "billingDetails": {
+            "name": "John Doe",
+            "phone": "+523988734644",
+            "addres": "Centro 123",
+            "city": "Monterrey",
+            "region": "Nuevo Leon",
+            "country": "Mexico"
+          }
+        },
+        "productDetails": {
+          "product": cartProductsG
+        }
+      }
+
+      $.ajax({
+        url: "https://api-uat.kushkipagos.com/card/v1/charges",
+        type: "POST",
+        headers: {
+          "Private-Merchant-I": privateKey
+        },
+        data: payload,
+        success: function(response) {
+            $('#message').html('Tu pago ha sido exitoso')
+            swal.hideLoading()
+            swal.close();
+          console.log(response);
+          localStorage.removeItem('cart')
+        },
+        error: function(xhr, status, error) {
+            $('#message').html('Tu pago no pudo ser procesado')
+            swal.hideLoading()
+            swal.close();
+          console.log(error);
+        }
+      });
+}
+
+function payTransfer(token){
+    const privateKey = privateCredentials();
+    var payload = {
+        "token": token,
+        "amount": {
+          "subtotalIva": 0,
+          "subtotalIva0": (totalG * 0.81).toFixed(2),
+          "iva": (totalG * 0.19).toFixed(2)
+        }
+      }
+
+      $.ajax({
+        url: "https://api-uat.kushkipagos.com/transfer/v1/init",
+        type: "POST",
+        headers: {
+          "Private-Merchant-I": privateKey
+        },
+        data: payload,
+        success: function(response) {
+            $('#message').html('Tu pago está en proceso')
+            swal.hideLoading()
+            swal.close();
+          console.log(response);
+          localStorage.removeItem('cart')
+        },
+        error: function(xhr, status, error) {
+            $('#message').html('Tu pago no pudo ser procesado')
+            swal.hideLoading()
+            swal.close();
+          console.log(error);
+        }
+      });
+}
+
+function payCash(token){
+    const privateKey = privateCredentials();
+    var payload = {
+        "token": token,
+        "amount": {
+          "subtotalIva": 0,
+          "subtotalIva0": (totalG * 0.81).toFixed(2),
+          "iva": (totalG * 0.19).toFixed(2)
+        }
+      }
+
+      $.ajax({
+        url: "https://api-uat.kushkipagos.com/cash/v1/charges/init",
+        type: "POST",
+        headers: {
+          "Private-Merchant-I": privateKey
+        },
+        data: payload,
+        success: function(response) {
+            $('#message').html('Tu pago está en proceso')
+            swal.hideLoading()
+            swal.close();
+          console.log(response);
+          localStorage.removeItem('cart')
+        },
+        error: function(xhr, status, error) {
+            $('#message').html('Tu pago no pudo ser procesado')
+            swal.hideLoading()
+            swal.close();
+          console.log(error);
+        }
+      });
+}
